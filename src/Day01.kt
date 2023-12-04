e033b95eb75e467dcec20d3039f64fb832c2cbce
Andrii <andriyo@gmail.com>
2023-12-03 23:14:09 -0800

    day 1 non-mutable idiomatic Kotlin

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,58 @@
+val digitNames = listOf(
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+)
+
+fun findFirst(s: String): String {
+    return listOfNotNull(
+        s.indexOfFirst {
+            it.isDigit()
+        }.takeIf { it > -1 }?.let { it to s[it].toString() },
+        digitNames.mapIndexed { digitIndex, digitName ->
+            val matchIndex = s.indexOf(digitName)
+            matchIndex to (digitIndex + 1).toString()
+        }.filterNot { it.first == -1 }.minByOrNull { it.first }).minBy { it.first }.second
+}
+
+fun findLast(s: String): String {
+    return listOfNotNull(
+        s.indexOfLast {
+            it.isDigit()
+        }.takeIf { it > -1 }?.let { it to s[it].toString() },
+        digitNames.mapIndexed { digitIndex, digitName ->
+            val matchIndex = s.lastIndexOf(digitName)
+            matchIndex to (digitIndex + 1).toString()
+        }.filterNot { it.first == -1 }.maxByOrNull { it.first }).maxBy { it.first }.second
+}
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map {
+            Integer.parseInt(
+                it.find { it.isDigit() }.toString() + it.findLast { it.isDigit() }
+            )
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { s ->
+            Integer.parseInt(findFirst(s) + findLast(s))
+        }.sum()
     }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    check(part1(testInput) == 142)
+    part2(testInput).println()
 
     val input = readInput("Day01")
     part1(input).println()
